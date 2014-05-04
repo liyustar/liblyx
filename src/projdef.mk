58f41e242219c9c3eee1baa928c246fbaacc559b
liyustar <liyustar@gmail.com>
2014-05-04 23:58:57 +0800

    可用一个Makefile来构建工程

--- src/projdef.mk
+++ src/projdef.mk
@@ -13,11 +13,13 @@ MODULES	:=	util \
 CPPFLAGS := -g -Wall
 CPPFLAGS += $(addprefix -I../,$(MODULES))
 
+all:
+
 $(PRODUCT): $(OBJ)
 	$(AR) $(ARFLAGS) $@ $<
 
 %.d: %.cpp
-	set -e;rm -f $@; \
+	@set -e;rm -f $@; \
 	$(CXX) -MM $(CPPFLAGS) $< > $@.$$$$; \
 	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
 	rm -f $@.$$$$
