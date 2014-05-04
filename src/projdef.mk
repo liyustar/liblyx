36ab1f9a29171ad836ce30717c715898e3998f08
liyustar <liyustar@gmail.com>
2014-05-04 23:24:55 +0800

    将projman工程的Makefile文件搬过来

---
+++ src/projdef.mk
@@ -0,0 +1,22 @@
+# 给module调用的，所以目录是上上级
+
+INCDIR := ../../include
+LIBDIR := ../../lib
+
+PRODUCT := $(LIBDIR)/liblyx.a
+
+# add modules
+MODULES := module_1 \
+			module_2
+
+CPPFLAGS := -g -Wall
+CPPFLAGS += $(addprefix -I../,$(MODULES))
+
+$(PRODUCT): $(OBJ)
+	$(AR) $(ARFLAGS) $@ $<
+
+%.d: %.cpp
+	set -e;rm -f $@; \
+	$(CXX) -MM $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
