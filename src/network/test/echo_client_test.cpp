428ebd2e69713bbc9003d73a2237d746bfd44def
liyustar <liyustar@gmail.com>
2014-05-09 00:10:16 +0800

    添加network模块测试程序

---
+++ src/network/test/echo_client_test.cpp
@@ -0,0 +1,29 @@
+#include "lyxSocket.h"
+#include <netdb.h>	// For in_port_t
+#include <stdio.h>
+#include <string.h>
+
+using namespace lyx;
+
+static const in_port_t listen_port = 8888;
+
+int main() {
+	TCPSocket clnt("localhost", listen_port);
+
+	char buf[] = "hello world";
+	size_t ret = clnt.send(buf, sizeof(buf));
+	if (ret != sizeof(buf)) {
+		perror("send() failed");
+	}
+
+	memset(buf, 0, sizeof(buf));
+
+	ret = clnt.recv(buf, sizeof(buf));
+	if (ret < 0) {
+		perror("recv() failed");
+	}
+
+	printf("success, recv: |%s|\n", buf);
+
+	return 0;
+}
