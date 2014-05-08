f9802214c947f2849f53d56a10dad9e42dfd4df3
liyustar <liyustar@gmail.com>
2014-05-09 00:08:32 +0800

    修改testdef.mk，要加入openssl库参数

--- src/testdef.mk
+++ src/testdef.mk
@@ -6,11 +6,19 @@ LIBDIR := ../../../lib
 PRODUCT := $(LIBDIR)/liblyx.a
 
 # add modules
-MODULES := module_1 \
-			module_2
+MODULES	:=	util \
+			network \
+			http
 
 CPPFLAGS := -g -Wall
 CPPFLAGS += $(addprefix -I../../,$(MODULES))
 
+# 其他依赖库头文件
+CPPFLAGS	+=	-I/usr/local/ssl/include
+
 LDFLAGS := -L$(LIBDIR)
 LDLIBS := -llyx
+
+# 其他依赖库
+LDFLAGS += -L/usr/local/ssl/lib
+LDLIBS += -lssl -lcrypto -ldl
