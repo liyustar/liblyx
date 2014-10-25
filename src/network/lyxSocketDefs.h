94c2f0ee032d8a1741a992967581d8b67ba7902e
liyustar <liyustar@gmail.com>
2014-10-25 23:23:37 +0800

    add lyxSocketDefs.h

---
+++ src/network/lyxSocketDefs.h
@@ -0,0 +1,13 @@
+#ifndef LIBLYX_NETWORK_LYXSOCKETDEFS_H_
+#define LIBLYX_NETWORK_LYXSOCKETDEFS_H_
+
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#endif // LIBLYX_NETWORK_LYXSOCKETDEFS_H_
