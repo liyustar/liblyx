5142ab90eef0773c75f5ac7c5c81829dd9cc657c
liyustar <liyustar@gmail.com>
2014-11-08 23:45:01 +0800

    增加Socket的测试用例

--- src/network/test/socket_test.cpp
+++ src/network/test/socket_test.cpp
@@ -1,4 +1,5 @@
 #include "lyxSocketImpl.h"
+#include "lyxSocket.h"
 #include "gtest/gtest.h"
 #include <string>
 
@@ -6,6 +7,21 @@ using namespace lyx;
 
 TEST(SocketImplTest, connect) {
     SocketImpl sock;
+    std::string request = "GET / HTTP/1.0\r\n\r\n";
+    char buf[1024];
+
+    sock.connect(SocketAddress("localhost", 80));
+
+    sock.sendBytes(request.c_str(), request.length());
+    int len = sock.receiveBytes(buf, 1024);
+    std::string response(buf, len);
+    int pos = response.find("\r\n\r\n") + 4;
+
+    EXPECT_EQ("hello world.\n", response.substr(pos));
+}
+
+TEST(SocketTest, connect) {
+    Socket sock;
     std::string request = "GET / HTTP/1.0\r\n\r\n";
     char buf[1024];
 
