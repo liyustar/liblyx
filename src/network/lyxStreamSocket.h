cf27219de2f254d833ebf516912a9b1446044746
liyustar <liyustar@gmail.com>
2014-11-08 23:59:35 +0800

    add lyxStreamSocket.h

---
+++ src/network/lyxStreamSocket.h
@@ -0,0 +1,41 @@
+#ifndef LIBLYX_NETWORK_LYXSTREAMSOCKET_H_
+#define LIBLYX_NETWORK_LYXSTREAMSOCKET_H_
+
+#include "lyxSocket.h"
+#include <memory>
+
+namespace lyx {
+
+class StreamSocket : public Socket {
+    public:
+        typedef std::shared_ptr<StreamSocketImpl> Ptr;
+
+        StreamSocket();
+        explicit StreamSocket(const SocketAddress& address);
+        explicit StreamSocket(IPAddress::Family family);
+        StreamSocket(const Socket& socket);
+
+        virtual ~StreamSocket();
+
+        StreamSocket& operator = (const Socket& socket);
+
+        void connect(const StreamAddress& address);
+
+        void shutdownReceive();
+        void shutdownSend();
+        void shutdown();
+
+        int sendBytes(const void* buffer, int length, int flags = 0);
+        int receiveBytes(void* buffer, int length, int flags = 0);
+
+        StreamSocket(Ptr pImpl);
+
+    private:
+        enum {
+            BUFFER_SIZE = 1024
+        };
+};
+
+} // namespace lyx
+
+#endif // LIBLYX_NETWORK_LYXSTREAMSOCKET_H_
