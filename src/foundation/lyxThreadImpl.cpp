74bc911cc481a798a3d0e2c5dc58c6b5418fdeb5
liyustar <liyustar@gmail.com>
2014-11-26 23:50:02 +0800

    增加ThreadImpl部分实现

--- src/foundation/lyxThreadImpl.cpp
+++ src/foundation/lyxThreadImpl.cpp
@@ -45,4 +45,84 @@ ThreadImpl::~ThreadImpl() {
     }
 }
 
+void ThreadImpl::setPriorityImpl(int prio) {
+    if (prio != _pData->prio) {
+        _pData->prio = prio;
+        _pData->policy = SCHED_OTHER;
+        if (isRunningImpl()) {
+            struct sched_param par;
+            par.sched_priority = mapPrio(_pData->prio, SCHED_OTHER);
+            if (pthread_setschedparam(_pData->thread, SCHED_OTHER, &par))
+                throw SystemException("cannot set thread priority");
+        }
+    }
 }
+
+void ThreadImpl::setOSPriorityImpl(int prio, int policy) {
+    if (prio != _pData->osPrio || policy != _pData->policy) {
+        if (_pData->pRunnableTarget || _pData->pCallbackTarget) {
+            struct sched_param par;
+            par.sched_priority = prio;
+            if (pthread_setschedparam(_pData->thread, policy, &par))
+                throw SystemException("cannot set thread priority");
+        }
+        _pData->prio   = reverseMapPrio(prio, policy);
+        _pData->osPrio = prio;
+        _pData->policy = policy;
+    }
+}
+
+int ThreadImpl::getMinOSPriorityImpl(int policy) {
+    return sched_get_priority_min(policy);
+}
+
+int ThreadImpl::getMaxOSPriorityImpl(int policy) {
+    return sched_get_priority_max(policy);
+}
+
+void ThreadImpl::setStackSizeImpl(int size) {
+    _pData->stackSize = 0;
+}
+
+int ThreadImpl::mapPrio(int prio, int policy) {
+    int pmin = getMinOSPriorityImpl(policy);
+    int pmax = getMaxOSPriorityImpl(policy);
+
+    switch (prio) {
+        case PRIO_LOWEST_IMPL:
+            return pmin;
+        case PRIO_LOW_IMPL:
+            return pmin + (pmax - pmin)/4;
+        case PRIO_NORMAL_IMPL:
+            return pmin + (pmax - pmin)/2;
+        case PRIO_HIGH_IMPL:
+            return pmin + 3*(pmax - pmin)/4;
+        case PRIO_HIGHEST_IMPL:
+            return pmax;
+        default:
+            ;   // BUG: invalid thread priority
+    }
+    return -1;  // just to satisfy compiler - we'll never get here anyway
+}
+
+int ThreadImpl::reverseMapPrio(int prio, int policy) {
+    if (policy == SCHED_OTHER) {
+        int pmin = getMinOSPriorityImpl(policy);
+        int pmax = getMaxOSPriorityImpl(policy);
+        int normal = pmin + (pmax - pmin)/2;
+        if (prio == pmax)
+            return PRIO_HIGHEST_IMPL;
+        else if (prio > normal)
+            return PRIO_HIGH_IMPL;
+        else if (prio == normal)
+            return PRIO_NORMAL_IMPL;
+        else if (prio > pmin)
+            return PRIO_LOW_IMPL;
+        else
+            return PRIO_LOWEST_IMPL;
+    }
+    else
+        return PRIO_HIGHEST_IMPL;
+}
+
+} // namespace lyx
