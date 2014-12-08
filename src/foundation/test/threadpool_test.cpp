0dcdd425b458382c5beb8518189f23270a88de88
liyustar <liyustar@gmail.com>
2014-12-08 23:48:05 +0800

    增加ThreadPoolTest测试

--- src/foundation/test/threadpool_test.cpp
+++ src/foundation/test/threadpool_test.cpp
@@ -7,7 +7,30 @@
 
 using namespace lyx;
 
-TEST(ThreadPoolTest, threadpool) {
+class ThreadPoolTest: public testing::Test {
+    public:
+        void count() {
+            _event.wait();
+            for (int i = 0; i < 10000; i++) {
+                _mutex.lock();
+                _count++;
+                _mutex.unlock();
+            }
+        }
+
+    protected:
+        virtual void SetUp() {
+            _event.reset();
+            _count = 0;
+        }
+
+    private:
+        lyx::FastMutex _mutex;
+        lyx::Event _event;
+        int _count;
+};
+
+TEST_F(ThreadPoolTest, threadpool) {
     ThreadPool pool(2, 3, 3);
     pool.setStackSize(1);
 
@@ -21,4 +44,30 @@ TEST(ThreadPoolTest, threadpool) {
     EXPECT_EQ(0, pool.used());
     EXPECT_EQ(4, pool.capacity());
     EXPECT_EQ(4, pool.available());
+
+    RunnableAdapter<ThreadPoolTest> ra(*this, &ThreadPoolTest::count);
+
+    pool.start(ra);
+    EXPECT_EQ(2, pool.allocated());
+    EXPECT_EQ(1, pool.used());
+    EXPECT_EQ(4, pool.capacity());
+    EXPECT_EQ(3, pool.available());
+
+    pool.start(ra);
+    EXPECT_EQ(2, pool.allocated());
+    EXPECT_EQ(2, pool.used());
+    EXPECT_EQ(4, pool.capacity());
+    EXPECT_EQ(2, pool.available());
+
+    pool.start(ra);
+    EXPECT_EQ(3, pool.allocated());
+    EXPECT_EQ(3, pool.used());
+    EXPECT_EQ(4, pool.capacity());
+    EXPECT_EQ(1, pool.available());
+
+    pool.start(ra);
+    EXPECT_EQ(4, pool.allocated());
+    EXPECT_EQ(4, pool.used());
+    EXPECT_EQ(4, pool.capacity());
+    EXPECT_EQ(0, pool.available());
 }
