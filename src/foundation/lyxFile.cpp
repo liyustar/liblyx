b61bc766244d543647a318735afa1c1b61dfb893
liyustar <liyustar@gmail.com>
2014-12-18 21:51:22 +0800

    增加了File的部分接口和实现

--- src/foundation/lyxFile.cpp
+++ src/foundation/lyxFile.cpp
@@ -1,6 +1,169 @@
 #include "lyxFile.h"
+#include "lyxException.h"
+#include <cassert>
 
 namespace lyx {
+
+File::File() {
+}
+
+File::File(const std::string& path): FileImpl(path) {
+}
+
+File::File(const char* path): FileImpl(std::string(path)) {
+}
+
+File::File(const Path& path): FileImpl(path.toString()) {
+}
+
+File::File(const File& file): FileImpl(file.getPathImpl()) {
+}
+
+File::~File() {
+}
+
+File& File::operator = (const File& file) {
+    setPathImpl(path);
+    return *this;
+}
+
+File& File::operaotr = (const char* path) {
+    assert (path);
+    setPathImpl(path);
+    return *this;
+}
+
+File& File::operator = (const Path& path) {
+    setPathImpl(path.toString());
+    return *this;
+}
+
+void File::swap(File& file) {
+    swapImpl(file);
+}
+
+bool File::exists() const {
+    return existsImpl();
+}
+
+bool File::canRead() const {
+    return canReadImpl();
+}
+
+bool File::canWrite() const {
+    return canWriteImpl();
+}
+
+bool File::canExecute() const {
+    return canExecuteImpl();
+}
+
+bool File::isFile() const {
+    return isFileImpl();
+}
+
+bool File::isLink() const {
+    return isLinkImpl();
+}
+
+bool File::isDirectory() const {
+    return isDirectoryImpl();
+}
+
+bool File::isDevice() const {
+    return isDeviceImpl();
+}
+
+bool File::isHidden() const {
+    return isHiddenImpl();
+}
+
+Timestamp File::created() const {
+    return createdImpl();
+}
+
+Timestamp File::getLastModified() const {
+    return getLastModifiedImpl();
+}
+
+File& File::setLastModified(const Timestamp& ts) {
+    setLastModifiedImpl(ts);
+    return *this;
+}
+
+File::FileSize File::getSize() const {
+    return getSizeImpl();
+}
+
+File& File::setSize(FileSize size) {
+    setSizeImpl(size);
+    return *this;
+}
+
+File& File::setWriteable(bool flag) {
+    setWriteableImpl(flag);
+    return *this;
+}
+
+File& File::setReadOnly(bool flag) {
+    setReadOnlyImpl(!flag);
+    return *this;
+}
+
+File& File::setExecutable(bool flag) {
+    setExecutableImpl(flag);
+    return *this;
+}
+
+void File::copyTo(const std::string& path) const {
+    throw NotImplementedException();
+}
+
+void File::copyDirecotry(const std::string& path) const {
+    throw NotImplementedException();
+}
+
+void File::moveTo(const std::string& path) {
+    copyTo(path);
+    remove(true);
+    setPathImpl(path);
+}
+
+void File::renameTo(const std::string& path) {
+    renameToImpl(path);
+    setPathImpl(path);
+}
+
+void File::remove(bool recursive) {
+    if (recursive && !isLink() && isDirectory()) {
+        std::vector<File> files;
+        list(files);
+        for (std::vector<File>::iterator it = files.begin(); it != files.end(); it++) {
+            it->remove(true);
+        }
+    }
+    removeImpl();
+}
+
+bool File::createFile() {
+    return createFileImpl();
+}
+
+bool File::createDirectory() {
+    return createDirectoryImpl();
+}
+
+void File::createDirectories() {
+    throw NotImplementedException();
+}
+
+void File::list(std::vector<std::string>& files) const {
+    throw NotImplementedException();
+}
+
+void File::list(std::vector<File>& files) const {
+    throw NotImplementedException();
+}
 
 void File::handleLastError(const std::string& path) {
     handleLastErrorImpl(path);
--- src/foundation/lyxFile.h
+++ src/foundation/lyxFile.h
@@ -2,13 +2,107 @@
 #define LIBLYX_FOUNDATION_LYXFILE_H_
 
 #include "lyxFileImpl.h"
+#include "lyxTimestamp.h"
+#include <vector>
 
 namespace lyx {
 
+class Path;
+
 class File: public FileImpl {
     public:
+        typedef FileSizeImpl FileSize;
+
+        File();
+        File(const std::string& path);
+        File(const char* path);
+        File(const Path& path);
+        File(const File& file);
+        virtual ~File();
+
+        File& operator = (const File& file);
+        File& operator = (const std::string& path);
+        File& operator = (const char* path);
+        File& operator = (const Path& path);
+
+        void swap(File& file);
+        const std::string& path() const;
+        bool exists() const;
+        bool canRead() const;
+        bool canWrite() const;
+        bool canExecute() const;
+
+        bool isFile() const;
+        bool isLink() const;
+        bool isDirectory() const;
+        bool isDevice() const;
+        bool isHidden() const;
+
+        Timestamp created() const;
+        Timestamp getLastModified() const;
+        File& setLastModified(const Timestamp& ts);
+
+        FileSize getSize() const;
+        File& setSize(FileSize size);
+        File& setWriteable(bool flag = true);
+        File& setReadOnly(bool flag = true);
+        File& setExecutable(bool flag = true);
+
+        void copyTo(const std::string& path) const;
+        void moveTo(const std::string& path);
+        void renameTo(const std::string& path);
+        void remove(bool recursive = false);
+
+        bool createFile();
+        bool createDirectory();
+        void createDirectories();
+        void list(std::vector<std::string>& files) const;
+        void list(std::vector<File>& files) const;
+
+        bool operator == (const File& file) const;
+        bool operator != (const File& file) const;
+        bool operator <  (const File& file) const;
+        bool operator <= (const File& file) const;
+        bool operator >  (const File& file) const;
+        bool operator >= (const File& file) const;
+
         static void handleLastError(const std::string& path);
+
+    protected:
+        void copyDirecotry(const std::string& path) const;
 };
+
+inline const std::string& File::path() const {
+    return getPathImpl();
+}
+
+inline bool File::operator == (const File& file) const {
+    return getPathImpl() == file.getPathImpl();
+}
+
+inline bool File::operator != (const File& file) const {
+    return getPathImpl() != file.getPathImpl();
+}
+
+inline bool File::operator < (const File& file) const {
+    return getPathImpl() < file.getPathImpl();
+}
+
+inline bool File::operator <= (const File& file) const {
+    return getPathImpl() <= file.getPathImpl();
+}
+
+inline bool File::operator > (const File& file) const {
+    return getPathImpl() > file.getPathImpl();
+}
+
+inline bool File::operator >= (const File& file) const {
+    return getPathImpl() >= file.getPathImpl();
+}
+
+inline void swap(File& f1, File& f2) {
+    f1.swap(f2);
+}
 
 } // namespace lyx
 
