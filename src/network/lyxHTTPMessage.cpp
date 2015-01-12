291314a17fbd234cc8ff35ccac31c3ad57738cd8
liyustar <liyustar@gmail.com>
2015-01-12 23:22:08 +0800

    add lyxHTTPMessage

---
+++ src/network/lyxHTTPMessage.cpp
@@ -0,0 +1,109 @@
+#include "lyxHTTPMessage.h"
+#include "lyxNumberFormatter.h"
+
+namespace lyx {
+
+const std::string HTTPMessage::HTTP_1_0 = "HTTP/1.0";
+const std::string HTTPMessage::HTTP_1_1 = "HTTP/1.1";
+
+const std::string HTTPMessage::IDENTITY_TRANSFER_ENCODING = "identity";
+const std::string HTTPMessage::CHUNKED_TRANSFER_ENCODING  = "chunked";
+
+const int         HTTPMessage::UNKNOWN_CONTENT_LENGTH     = -1;
+const std::string HTTPMessage::UNKNOWN_CONTENT_TYPE;
+
+const std::string HTTPMessage::CONTENT_LENGTH    = "Content-Length";
+const std::string HTTPMessage::CONTENT_TYPE      = "Content-Type";
+const std::string HTTPMessage::TRANSFER_ENCODING = "Transfer-Encoding";
+const std::string HTTPMessage::CONNECTION        = "Connection";
+
+const std::string HTTPMessage::CONNECT_KEEP_ALIVE = "Keep-Alive";
+const std::string HTTPMessage::CONNECT_CLOSE      = "Close";
+
+const std::string HTTPMessage::EMPTY;
+
+HTTPMessage::HTTPMessage():
+    _version(HTTP_1_0)
+{
+}
+
+HTTPMessage::HTTPMessage(const std::string& version):
+    _version(version)
+{
+}
+
+HTTPMessage::~HTTPMessage() {
+}
+
+void HTTPMessage::setVersion(const std::string& version) {
+    _version = version;
+}
+
+void HTTPMessage::setContentLength(std::streamsize length) {
+    if (length != UNKNOWN_CONTENT_LENGTH)
+        set(CONTENT_LENGTH, NumberFormatter::format(length));
+    else
+        erase(CONTENT_LENGTH);
+}
+
+std::streamsize HTTPMessage::getContentLength() const {
+    const std::string& contentLength = get(CONTENT_LENGTH, EMPTY);
+    if (!contentLength.empty())
+        return static_cast<std::streamsize>(std::atoi(contentLength.c_str()));
+    else
+        return UNKNOWN_CONTENT_LENGTH;
+}
+
+void HTTPMessage::setTransferEncoding(const std::string& transferEncoding) {
+    if (transferEncoding.compare(IDENTITY_TRANSFER_ENCODING) == 0)
+        erase(TRANSFER_ENCODING);
+    else
+        set(TRANSFER_ENCODING, transferEncoding);
+}
+
+const std::string& HTTPMessage::getTransferEncoding() const {
+    return get(TRANSFER_ENCODING, IDENTITY_TRANSFER_ENCODING);
+}
+
+void HTTPMessage::setChunkedTransferEncoding(bool flag) {
+    if (flag)
+        setTransferEncoding(CHUNKED_TRANSFER_ENCODING);
+    else
+        setTransferEncoding(IDENTITY_TRANSFER_ENCODING);
+}
+
+bool HTTPMessage::getChunkedTransferEncoding() const {
+    return getTransferEncoding().compare(CHUNKED_TRANSFER_ENCODING) == 0;
+}
+
+void HTTPMessage::setContentType(const std::string& mediaType) {
+    if (mediaType.empty())
+        erase(CONTENT_TYPE);
+    else
+        set(CONTENT_TYPE, mediaType);
+}
+
+void HTTPMessage::setContentType(const MediaType& mediaType) {
+    setContentType(mediaType.toString());
+}
+
+const std::string& HTTPMessage::getContentType() const {
+    return get(CONTENT_TYPE, UNKNOWN_CONTENT_TYPE);
+}
+
+void HTTPMessage::setKeepAlive(bool keepAlive) {
+    if (keepAlive)
+        set(CONNECTION, CONNECT_KEEP_ALIVE);
+    else
+        set(CONNECTION, CONNECT_CLOSE);
+}
+
+bool HTTPMessage::getKeepAlive() const {
+    const std::string& connection = get(CONNECTION, EMPTY);
+    if (!connection.empty())
+        return connection.compare(CONNECT_CLOSE) != 0;
+    else
+        return getVersion() == HTTP_1_1;
+}
+
+} // namespace lyx
---
+++ src/network/lyxHTTPMessage.h
@@ -0,0 +1,72 @@
+#ifndef LIBLYX_NETWORK_LYXHTTPMESSAGE_H_
+#define LIBLYX_NETWORK_LYXHTTPMESSAGE_H_
+
+#include "lyxMessageHeader.h"
+
+namespace lyx {
+
+class MediaType;
+
+class HTTPMessage: public MessageHeader {
+    public:
+        void setVersion(const std::string& version);
+        const std::string& getVersion() const;
+
+        void setContentLength(std::streamsize length);
+        std::streamsize getContentLength() const;
+        bool hasContentLength() const;
+
+        void setTransferEncoding(const std::string& transferEncoding);
+        const std::string& getTransferEncoding() const;
+
+        void setChunkedTransferEncoding(bool flag);
+        bool getChunkedTransferEncoding() const;
+
+        void setContentType(const std::string& mediaType);
+        const std::string& getContentType() const;
+
+        void setKeepAlive(bool keepAlive);
+        bool getKeepAlive() const;
+
+        static const std::string HTTP_1_0;
+        static const std::string HTTP_1_1;
+
+        static const std::string IDENTITY_TRANSFER_ENCODING;
+        static const std::string CHUNKED_TRANSFER_ENCODING;
+
+        static const int         UNKNOWN_CONTENT_LENGTH;
+        static const std::string UNKNOWN_CONTENT_TYPE;
+
+        static const std::string CONTENT_LENGTH;
+        static const std::string CONTENT_TYPE;
+        static const std::string TRANSFER_ENCODING;
+        static const std::string CONNECTION;
+
+        static const std::string CONNECT_KEEP_ALIVE;;
+        static const std::string CONNECT_CLOSE;
+
+        static const std::string EMPTY;
+
+    protected:
+        HTTPMessage();
+        HTTPMessage(const std::string& version);
+        virtual ~HTTPMessage();
+
+    private:
+        HTTPMessage(const HTTPMessage&);
+        HTTPMessage& operator = (const HTTPMessage&);
+
+        std::string _version;
+};
+
+inline const std::string& HTTPMessage::getVersion() const {
+    return _version;
+}
+
+inline bool HTTPMessage::hasContentLength() const {
+    return has(CONTENT_LENGTH);
+}
+
+} // namespace lyx
+
+#endif // LIBLYX_NETWORK_LYXHTTPMESSAGE_H_
